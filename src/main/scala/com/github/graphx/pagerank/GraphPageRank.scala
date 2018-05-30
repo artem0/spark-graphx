1d009e10c9fdd9060b74b553a5d2d9761fe0ceba
arukavytsia <artem.rukavitsya@gmail.com>
2018-05-30 22:31:33 +0300

    Polish Page Rank

--- src/main/scala/com/github/graphx/pagerank/GraphPageRank.scala
+++ src/main/scala/com/github/graphx/pagerank/GraphPageRank.scala
@@ -5,34 +5,40 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx.VertexRDD
 
-import scala.compat.Platform.EOL
-
-object GraphPageRank extends App {
-  Logger.getLogger("org").setLevel(Level.ERROR)
-  val sc = new SparkContext("local[*]", "GraphPageRankDemo")
-
-  val socialGraph = new SocialGraph(sc)
-  val tolerance = 0.0001
+object GraphPageRank {
 
   /**
     * Dynamic implementation uses the Pregel interface and runs PageRank until convergence
     */
-  val ranks: VertexRDD[Double] = socialGraph.graph.pageRank(tol = tolerance).vertices
+  def ranks(socialGraph: SocialGraph, tolerance: Double): VertexRDD[Double] =
+    socialGraph.graph.pageRank(tol = tolerance).vertices
 
   /**
     * The standalone Graph interface and runs PageRank for a fixed number of iterations
     */
-  val static: VertexRDD[Double] = socialGraph.graph.staticPageRank(numIter = 20).vertices
+  def static(socialGraph: SocialGraph, tolerance: Double): VertexRDD[Double] =
+    socialGraph.graph.staticPageRank(numIter = 20).vertices
 
-  def handleResult(ranks: VertexRDD[Double]) = {
+  def handleResult(socialGraph: SocialGraph, ranks: VertexRDD[Double]) = {
     socialGraph.verts.join(ranks).map {
       case (_, (username, rank)) => (username, rank)
     }.sortBy({ case (_, rank) => rank }, ascending = false).take(10)
   }
 
-  val topUsersDynamically = handleResult(ranks).mkString(EOL)
-  println(s"Top 10 users in network (counted with tolerance until convergence $tolerance):$EOL$topUsersDynamically$EOL")
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "PageRank")
 
-  val topUsersIterative = handleResult(ranks).mkString(EOL)
-  println(s"Top 10 users in network (counted iterative):$EOL$topUsersDynamically$EOL")
+    val socialGraph: SocialGraph = new SocialGraph(sc)
+    val tolerance: Double = 0.0001
+
+    import scala.compat.Platform.{EOL => D}
+    val topUsersDynamically = handleResult(socialGraph, ranks(socialGraph, tolerance)).mkString(D)
+    val topUsersIterative = handleResult(socialGraph, static(socialGraph, tolerance)).mkString(D)
+
+    println(s"Top 10 users in network counted with tolerance until convergence $tolerance - $D $topUsersDynamically")
+    println(s"Top 10 users in the network counted iteratively - $D $topUsersIterative")
+
+    sc.stop()
+  }
 }
